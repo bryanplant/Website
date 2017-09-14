3ccd24ea620aa0d98fd7a629e0ddb1a561bc5ed3
Bryan Plant <bryanplant@gmail.com>
2017-09-13 22:27:12 -0600

    Delete code to make room for new style

--- bryanplantWebsite/web/main.dart
+++ bryanplantWebsite/web/main.dart
@@ -13,25 +13,5 @@ void main() {
     var height = window.innerHeight.toString() + "px";
     querySelector("#wrapper").style.height = height;
   });
-
-  box = [querySelector("#box1"), querySelector("#box2"), querySelector("#box3"), querySelector("#box4"),
-         querySelector("#box5"), querySelector("#box6"), querySelector("#box7"), querySelector("#box8"),
-         querySelector("#box9"), querySelector("#box10"), querySelector("#box11"), querySelector("#box12"),
-         querySelector("#box13"), querySelector("#box14"), querySelector("#box15"), querySelector("#box16")];
-
-  for(int i=0; i < box.length; i++){
-    box[i].style.float = "left";
-    box[i].style.width = "25%";
-    box[i].style.height = "25%";
-    box[i].style.backgroundColor = "blue";
-
-    box[i].onMouseOver.listen((MouseEvent e){
-      var r = rand.nextInt(255);
-      var g = rand.nextInt(255);
-      var b = rand.nextInt(255);
-      String color = "rgb($r,$g,$b)";
-      box[i].style.backgroundColor = color;
-    });
-  }
 }
 
