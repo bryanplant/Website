8a1b17dfa796216cbe57b615267c43ecca9afd7a
Bryan Plant <bryanplant@gmail.com>
2017-09-13 23:23:35 -0600

    Draws boxes of random color in random location using asyncronous commands

--- bryanplantWebsite/web/main.dart
+++ bryanplantWebsite/web/main.dart
@@ -1,17 +1,40 @@
 import "dart:html";
 import "dart:math";
-var rand;
-List<DivElement> box;
+import "dart:async";
 
-void main() {
+Random rand;
+CanvasElement canvas;
+CanvasRenderingContext2D c2d;
+
+
+Future main() async {
   rand = new Random();
+  canvas = querySelector("#canvas");
+  c2d = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-  var height = window.innerHeight.toString() + "px";
-  querySelector("#wrapper").style.height = height;
 
-  window.onResize.listen((Event e){
-    var height = window.innerHeight.toString() + "px";
-    querySelector("#wrapper").style.height = height;
+  drawRect(String color){
+    c2d.fillStyle = color;
+    c2d.fillRect(rand.nextInt(canvas.width-35), rand.nextInt(canvas.height-35), 30, 30);
+  }
+
+  Future draw(String color) async{
+    return new Future.delayed(const Duration(milliseconds: 250), () => drawRect(color));
+  }
+
+  for(int i = 0; i < 100; i ++) {
+    var r = rand.nextInt(255);
+    var g = rand.nextInt(255);
+    var b = rand.nextInt(255);
+    String color = "rgb($r,$g,$b)";
+    await draw(color);
+  }
+
+  window.onResize.listen((_){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
   });
 }
 
