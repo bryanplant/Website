b0dd146f90fa03d4bbc4c1f531b8790abc0ca1d8
Bryan Plant <bryanplant@gmail.com>
2017-09-17 21:36:47 -0600

    Ignore .idea and build directories

--- bryanplantWebsite/web/main.dart
+++ bryanplantWebsite/web/main.dart
@@ -3,33 +3,48 @@ import "dart:math";
 import "dart:async";
 
 Random rand;
-CanvasElement canvas;
-CanvasRenderingContext2D c2d;
-
+CanvasElement canvas = querySelector("#canvas");
+CanvasRenderingContext2D c2d = canvas.getContext('2d');
 
 Future main() async {
   rand = new Random();
-  canvas = querySelector("#canvas");
-  c2d = canvas.getContext('2d');
+
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
+  c2d.fillStyle = 'gray';
+  c2d.fillRect(0, 0, canvas.width, canvas.width);
 
-  drawRect(String color){
+  drawRect(int x, int y, String color){
     c2d.fillStyle = color;
-    c2d.fillRect(rand.nextInt(canvas.width-35), rand.nextInt(canvas.height-35), 30, 30);
+    c2d.beginPath();
+    c2d.arc(x, y, 10, 0, 2*PI, false);
+    c2d.closePath();
+    c2d.fill();
   }
 
-  Future draw(String color) async{
-    return new Future.delayed(const Duration(milliseconds: 250), () => drawRect(color));
+  deleteRect(int x, int y){
+    c2d.fillStyle = 'gray';
+    c2d.fillRect(x-10, y-10, 20, 20);
+  }
+
+  Future draw(int x, int y, String color){
+    return new Future.delayed(const Duration(milliseconds: 250), () => drawRect(x, y, color));
+  }
+
+  Future delete(int x, int y){
+    return new Future.delayed(const Duration(milliseconds: 3000), () => deleteRect(x, y));
   }
 
   for(int i = 0; i < 100; i ++) {
     var r = rand.nextInt(255);
     var g = rand.nextInt(255);
     var b = rand.nextInt(255);
+    var x = rand.nextInt(canvas.width-20);
+    var y = rand.nextInt(canvas.height-20);
     String color = "rgb($r,$g,$b)";
-    await draw(color);
+    await draw(x, y, color);
+    delete(x, y);
   }
 
   window.onResize.listen((_){
