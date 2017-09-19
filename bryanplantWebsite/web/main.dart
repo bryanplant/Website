8a7dcd4f7c452d8dc66550298a8f8386e1217d47
Bryan Plant <bryanplant@gmail.com>
2017-09-18 21:57:12 -0600

    Draws circles instead of stars
    Circles can vary in size

--- bryanplantWebsite/web/main.dart
+++ bryanplantWebsite/web/main.dart
@@ -8,40 +8,51 @@ CanvasRenderingContext2D c2d = canvas.getContext('2d');
 
 void main() {
   int numStars = 1000;
+  int numStarsDrawn = 0;
   List<int> starX = new List(numStars);
   List<int> starY = new List(numStars);
+  List<int> starWidth = new List(numStars);
+  List<String> starColor = new List(numStars);
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  c2d.fillStyle = 'gray';
-  c2d.fillRect(0, 0, canvas.width, canvas.width);
+  drawScreen() {
+    c2d.fillStyle = 'black';
+    c2d.fillRect(0, 0, canvas.width, canvas.width);
+    c2d.font = "48px sans-serif";
+    c2d.fillStyle = 'white';
+    c2d.fillText("Bryan Plant", 50, 50);
+  }
 
-  drawStar(int x, int y, String color, int i){
+  drawScreen();
+
+  drawStar(int x, int y, int starWidth, String color){
     c2d.fillStyle = color;
     c2d.beginPath();
-    c2d.arc(x, y, 10, 0, 2*PI, false);
+    c2d.lineTo(x, y-starWidth/2);
+    c2d.lineTo(x+starWidth/8, y-starWidth/8);
+    c2d.lineTo(x+starWidth/2, y);
+    c2d.lineTo(x+starWidth/8, y+starWidth/8);
+    c2d.lineTo(x, y+starWidth/2);
+    c2d.lineTo(x-starWidth/8, y+starWidth/8);
+    c2d.lineTo(x-starWidth/2, y);
+    c2d.lineTo(x-starWidth/8, y-starWidth/8);
     c2d.closePath();
     c2d.fill();
+  }
 
+  deleteRect(int x, int y, int width){
     c2d.fillStyle = 'black';
-    c2d.fillRect(20, 5, 250, 23);
-    c2d.fillStyle = 'white';
-    c2d.font = '24px serif';
-    c2d.fillText("Number of Circles: " + (i+1).toString(), 25, 25);
+    c2d.fillRect(x-width/2, y-width/2, width, width);
   }
 
-  deleteRect(int x, int y){
-    c2d.fillStyle = 'gray';
-    c2d.fillRect(x-10, y-10, 20, 20);
+  Future draw(int x, int y, int starWidth, String color){
+    return new Future.delayed(const Duration(milliseconds: 250), () => drawStar(x, y, starWidth, color));
   }
 
-  Future draw(int x, int y, String color, int i){
-    return new Future.delayed(const Duration(milliseconds: 100), () => drawStar(x, y, color, i));
-  }
-
-  Future delete(int x, int y){
-    return new Future.delayed(const Duration(milliseconds: 3000), () => deleteRect(x, y));
+  Future delete(int x, int y, int width){
+    return new Future.delayed(const Duration(milliseconds: 3000), () => deleteRect(x, y, width));
   }
 
   Future run() async {
@@ -51,23 +62,27 @@ void main() {
       int b = rand.nextInt(255);
       int x;
       int y;
+      int width = rand.nextInt(50)+20;
       bool valid = false;
       while (!valid) {
         valid = true;
-        x = rand.nextInt(canvas.width - 20);
-        y = rand.nextInt(canvas.height - 20);
+        x = rand.nextInt(canvas.width - width*2) + width;
+        y = rand.nextInt(canvas.height - width*2) + width;
         for (int j = 0; j < i; j++) {
-          if ((x - starX[j]).abs() < 20 && (y - starY[j]).abs() < 20) {
+          if ((x - starX[j]).abs() < starWidth[j] && (y - starY[j]).abs() < starWidth[j]) {
             valid = false;
             break;
           }
         }
-        starX[i] = x;
-        starY[i] = y;
       }
+      starX[i] = x;
+      starY[i] = y;
+      starWidth[i] = width;
       String color = "rgb($r,$g,$b)";
-      await draw(x, y, color, i);
-      //delete(x, y);
+      starColor[i] = color;
+      await draw(x, y, width, color);
+      delete(x, y, width);
+      numStarsDrawn++;
     }
 
     window.onResize.listen((_) {
@@ -75,6 +90,17 @@ void main() {
       canvas.height = window.innerHeight;
     });
   }
+
+  window.onResize.listen((e){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    drawScreen();
+
+    for(int i = 0; i < numStarsDrawn; i++){
+      drawStar(starX[i], starY[i], starWidth[i], starColor[i]);
+    }
+  });
 
   run();
 }
